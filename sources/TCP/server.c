6824a939a1953ee42677a99d1ede68027a2445c7
root <root@debian>
2015-10-19 19:14:54 +0300

    Смотреть пока нечего, но работа идет

--- sources/TCP/server.c
+++ sources/TCP/server.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <string.h>
 
 
 struct product{
@@ -12,52 +13,103 @@ struct product{
 	int amount;
 };
 
-void buy(){
-printf("buy product\n");
+struct product goods[10];
+int seNum=0;
+
+
+
+void add(int csock){
+char *name,*sprice,*samount;
+int n,id;
+printf("Enter product name:\n");
+n=recv(csock,name,1024,0);
+name[n]=0;
+strcpy (goods[seNum].name, name);
+printf("%s",name);
+
+printf("Enter product price:\n");
+n=recv(csock,sprice,1024,0);
+sprice[n]=0;
+//int price=atoi(sprice);
+printf("%s",sprice);
+//goods[seNum].price=price;
+
+printf("Enter product amount:\n");
+n=recv(csock,samount,1024,0);
+samount[n]=0;
+//int amount=atoi(samount);
+printf("%s",samount);
+//goods[seNum].amount=amount;
+
+goods[seNum].id=seNum;
+seNum++;
 }
 
-void showList(struct product list[]){
+void buy(int csock){
+char *str;
+int n;
+printf("Enter product name:\n");
+n=recv(csock,str,1024,0);
+str[n]=0;
+printf(" %s ",str);
+
+}
+
+
+
+
+void showList(){
 printf("This is the list of products:\n");
 printf("id	price	name\n");
 int i;
-for (i=0;i<7;i++){
-	printf("%i	%i	%s",list[i].id,list[i].price,list[i].name);
+for (i=0;i<seNum-1;i++){
+	printf("%i	%i	%s",goods[i].id,goods[i].price,goods[i].name);
 }
 }
 
 
-void dialog(struct product goods[]){    //принимаем команды клиента и решаем что дальше делать
-char str[15];
-int com;
-printf("Enter your command:\n");
-scanf("%s", str);
-if (!strcmp(str, "buy"))
+
+
+void dialog(int csock){
+int com,n;
+char *str;
+printf("\nEnter command:\n");
+n=recv(csock,str,1024,0);
+str[n]=0;
+printf("%s\n",str);
+if (!strcmp(str, "buy\r\n"))
         com=1;
-if (!strcmp(str, "list"))
+if (!strcmp(str, "list\r\n"))
         com=2;
+if (!strcmp(str, "add\r\n"))
+        com=3;
 switch (com ) {
 case 1:
-  buy();
+  buy(csock);
   break;
 case 2:
-  showList(goods);
+  showList();
+  break;
+case 3:
+  add(csock);
   break;
 default:
   printf("Incorrect entry. ");
-  dialog(goods);
+  dialog(csock);
   break;
 }
 }
 
 
-void servInit(){   //загружаем состояние магазина-информацию о товарах
+
+
+void servInit(){   
 FILE *id,*names,*prices,*amount;
 id = fopen("id.txt","r");
 names = fopen("names.txt","r");
 prices = fopen("prices.txt","r");
 amount = fopen("amount.txt","r");
 char st[35];
-struct product goods[10];
 int i=0;
 while ( !feof(names) ) {
 	fgets(goods[i].name,35,names);
@@ -65,21 +117,20 @@ while ( !feof(names) ) {
 	fscanf(prices,"%i",&goods[i].price);
 	fscanf(amount,"%i",&goods[i].amount);
 	i++;
+	seNum++;
 	}
-dialog(goods);
 }
+
 
 
 
 int main(int argc, char**argv[])
 {
-//	int lsock,csock,n;
-//	struct sockaddr_in serv_addr,cli_addr;
-//	socklen_t addr_len;
-//	char mesg[50];
-	
+	int lsock,csock,n;
+	struct sockaddr_in serv_addr,cli_addr;
+	socklen_t addr_len;
 	servInit();
-/*	return 0;
+	
 
 	lsock=socket(AF_INET,SOCK_STREAM,0);
 
@@ -95,20 +146,13 @@ int main(int argc, char**argv[])
 	csock=accept(lsock,(struct sockaddr *)&cli_addr,&addr_len);
 	close(lsock);
 
+	printf("Welcome to our e-shop!\n");
 	for (;;)
 	{
-		n = recvfrom(csock,mesg,50,0,(struct sockaddr *)&cli_addr,&addr_len);
- //   		sendto(csock,mesg,n,0,(struct sockaddr *)&cli_addr,sizeof(cli_addr));
-	
-		printf("Welcome to our e-shop!\n");
-		mesg[n] = 0;
-		server(mesg);
-        
-  //      	printf("Received the following:\n");
-    //    	printf("%s",mesg);
-      //  	printf("-------------------------------------------------------\n");
+		dialog(csock);
+
 	}
-	close(csock);*/
+	close(csock);
 }
 
 
